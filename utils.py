57be2dbd496470f72c2434d3465fa1aded491a6a
liujingyao730 <liujingyao@ict.ac.cn>
2020-11-13 15:25:03 +0800

    增加了一个不一定会用的工具

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.sparse as sp
 
 def sparselist_to_tensor(adj_list):
 
@@ -8,3 +9,11 @@ def sparselist_to_tensor(adj_list):
 
     return tensor
 
+def normalize_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
